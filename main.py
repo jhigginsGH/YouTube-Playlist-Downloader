9503be02921595fa125183addb78745d45839d27
jhiggins <jackson.higgins@icloud.com>
2024-07-03 23:17:51 -0400

    Started checking if songs have been downloaded based on a log file.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import googleapiclient.discovery
 import pytube
 import os
+import csv
 from required_var import YPD_API_KEY, YPD_PLAYLIST_ID
+
 
 API_KEY = YPD_API_KEY
 PLAYLIST_ID = YPD_PLAYLIST_ID
@@ -33,19 +35,44 @@ def get_playlist(playlist_id):
     
 def download_videos(video_ids):
     """Iterate over video IDs and downloads the video with that ID from youtube."""
-    
+
+    ids = []
+
+    log_path = './logs'
+    log_file = './logs/log.csv'
+
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+        with open(log_file, 'w') as file:
+            csv.writer(file, delimiter=',').writerow(['Placeholder'])
+   
+    with open(log_file, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            ids.append(row)
+
+    videos_to_append = []
+
     for video_id in video_ids:
         try:
-            url = f"https://www.youtube.com/watch?v={video_id}"
-            yt = pytube.YouTube(url)
-            stream = yt.streams.filter(progressive=True,
-                                       file_extension="mp4").order_by("resolution").desc().first()
-            stream.download(output_path=r'C:\YT Playlist')
-            print(f"Downloaded: {yt.title}")
+
+            if video_id in ids:
+                pass
+           
+            else:
+                url = f"https://www.youtube.com/watch?v={video_id}"
+                yt = pytube.YouTube(url)
+                stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
+                stream.download(output_path=r'C:\YT Playlist')
+                print(f"Downloaded: {yt.title}")
+                videos_to_append.append(video_id)
 
         except Exception as e:
             print(f"Failed to download {video_id}: {e}")
-
+    with open(log_file, 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(videos_to_append)
 
 video_ids = get_playlist(PLAYLIST_ID)
 download_videos(video_ids)
