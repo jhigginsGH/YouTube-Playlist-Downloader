0d760d83bd75a2308becedf17c04328ff22962ed
jhiggins <jackson.higgins@icloud.com>
2024-12-18 16:05:34 -0500

    Added comments and if name = main

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def get_playlist(playlist_id):
 
     youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=API_KEY)
 
+    # Gets list of videos and their properties in the playlist
     request = youtube.playlistItems().list(
         part="snippet",
         playlistId=playlist_id,
@@ -25,6 +26,7 @@ def get_playlist(playlist_id):
     while request is not None:
         response = request.execute()
         
+        # Extracts video ID and appends it the the list to use in the download request
         for item in response["items"]:
             videos.append(item["snippet"]["resourceId"]["videoId"])
         
@@ -41,6 +43,7 @@ def download_videos(video_ids):
     log_path = './logs'
     log_file = './logs/log.csv'
 
+    # Creates log path and file if none exist
     if not os.path.exists(log_path):
         os.mkdir(log_path)
         with open(log_file, 'w', newline='') as file:
@@ -55,9 +58,10 @@ def download_videos(video_ids):
     for video_id in video_ids:
         try:
 
+            # Checks if video ID is in the log file and downloads the video if it is not
             if [video_id] in ids:
                 print('Video already downloaded')
-           
+
             else:
                 url = f"https://www.youtube.com/watch?v={video_id}"
                 yt = pytube.YouTube(url)
@@ -69,11 +73,15 @@ def download_videos(video_ids):
         except Exception as e:
             print(f"Failed to download {video_id}: {e}")
 
+    # Logs the videos downloaded
     with open(log_file, 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(videos_to_append)
 
 video_ids = get_playlist(PLAYLIST_ID)
-download_videos(video_ids)
+
+# Downloads all videos from a public YouTube Playlist
+if __name__ == "__main__":
+    download_videos(video_ids)
 
 
